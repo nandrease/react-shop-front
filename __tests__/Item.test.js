d187743a7a894ec200c849feb1acc71d99c5bd22
nandrease <neeme.andreas.eller@gmail.com>
2019-09-26 18:17:00 +0300

    Snapshot testing

--- __tests__/Item.test.js
+++ __tests__/Item.test.js
@@ -1,5 +1,6 @@
 import ItemComponent from '../components/Item';
 import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 
 const fakeItem = {
 	id: '49EO',
@@ -11,24 +12,28 @@ const fakeItem = {
 };
 
 describe('<Item/>', () => {
-	it('renders and displays properly', () => {
+	it('renders and matches the snapshot', () => {
 		const wrapper = shallow(<ItemComponent item={fakeItem} />);
-		const PriceTag = wrapper.find('PriceTag');
-		const Title = wrapper.find('Title a');
-		const Img = wrapper.find('img');
-		expect(PriceTag.children().text()).toBe('20 €');
-		expect(Title.text()).toBe(fakeItem.title);
-		expect(Img.props().src).toBe(fakeItem.image);
-		expect(Img.props().alt).toBe(fakeItem.title);
+		expect(toJSON(wrapper)).toMatchSnapshot();
 	});
+	// it('renders and displays properly', () => {
+	// 	const wrapper = shallow(<ItemComponent item={fakeItem} />);
+	// 	const PriceTag = wrapper.find('PriceTag');
+	// 	const Title = wrapper.find('Title a');
+	// 	const Img = wrapper.find('img');
+	// 	expect(PriceTag.children().text()).toBe('20 €');
+	// 	expect(Title.text()).toBe(fakeItem.title);
+	// 	expect(Img.props().src).toBe(fakeItem.image);
+	// 	expect(Img.props().alt).toBe(fakeItem.title);
+	// });
 
-	it('renders out the buttons properly', () => {
-		const wrapper = shallow(<ItemComponent item={fakeItem} />);
-		const buttonList = wrapper.find('.buttonList');
-        // console.log(buttonList.debug());
-		expect(buttonList.children()).toHaveLength(3);
-        expect(buttonList.find('Link')).toHaveLength(1);
-        expect(buttonList.find('AddToCart')).toBeTruthy();
-        expect(buttonList.find('DeleteItem').exists()).toBe(true);
-	});
+	// it('renders out the buttons properly', () => {
+	// 	const wrapper = shallow(<ItemComponent item={fakeItem} />);
+	// 	const buttonList = wrapper.find('.buttonList');
+	// 	// console.log(buttonList.debug());
+	// 	expect(buttonList.children()).toHaveLength(3);
+	// 	expect(buttonList.find('Link')).toHaveLength(1);
+	// 	expect(buttonList.find('AddToCart')).toBeTruthy();
+	// 	expect(buttonList.find('DeleteItem').exists()).toBe(true);
+	// });
 });
