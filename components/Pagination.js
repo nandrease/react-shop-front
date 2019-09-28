d2022548e4e2e37e765b001e063599eb509d0ffe
nandrease <neeme.andreas.eller@gmail.com>
2019-09-28 10:56:32 +0300

    Testing pagination

--- components/Pagination.js
+++ components/Pagination.js
@@ -24,7 +24,7 @@ const Pagination = (props) => (
 			const pages = Math.ceil(count / perPage);
 			const page = props.page;
 			return (
-				<PaginationStyles>
+				<PaginationStyles data-test="pagination">
 					<Head>
 						<title>
 							Sick Fits ! | Page {page} of {pages}
@@ -36,7 +36,7 @@ const Pagination = (props) => (
 						</a>
 					</Link>
 					<p>
-						Page {page} of {pages}
+						Page {page} of <span className="totalPages">{pages}</span>
 					</p>
 					<p>{count} items total</p>
 					<Link prefetch href={{ pathname: 'items', query: { page: page + 1 } }}>
@@ -51,3 +51,4 @@ const Pagination = (props) => (
 );
 
 export default Pagination;
+export { PAGINATION_QUERY };
